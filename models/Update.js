29e5f75ce3574fa70ca284f6968c114bdca02147
Licky367 <licky367@gmail.com>
2026-04-30 09:34:04 +0300

    Update Update.js

--- models/Update.js
+++ models/Update.js
@@ -3,28 +3,44 @@
 const mongoose = require('mongoose');
 
 const updateSchema = new mongoose.Schema({
+
+  // Which dairy this update belongs to
   dairy: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Dairy',
-    required: true
+    required: true,
+    index: true
   },
 
+  // Who made the update
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true
   },
 
+  // Image snapshot (NEW image when changed)
   image: {
     type: String,
     default: null
   },
 
+  // Optional comment (FB-style caption or comment)
   comment: {
     type: String,
-    trim: true
+    trim: true,
+    maxlength: 500
+  },
+
+  // Type of update (important for filtering later)
+  type: {
+    type: String,
+    enum: ['image', 'comment'],
+    required: true
   }
 
-}, { timestamps: true });
+}, {
+  timestamps: true
+});
 
 module.exports = mongoose.model('Update', updateSchema);
