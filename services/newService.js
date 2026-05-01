131a336a9f7f14c7fc11b5fe002f5fe2af70ba06
Licky367 <licky367@gmail.com>
2026-05-01 11:32:54 +0300

    Create newService.js

---
+++ services/newService.js
@@ -0,0 +1,17 @@
+const Dairy = require("../models/dairy");
+
+// =========================
+// CREATE NEW RECORD
+// =========================
+exports.createDairyRecord = async (data) => {
+
+  const dairy = new Dairy({
+    name: data.name,
+    profileImage: data.profileImage || "",
+    dob: data.dob || null,
+    code: Number(data.code),
+    mass: Number(data.mass)
+  });
+
+  return await dairy.save();
+};
