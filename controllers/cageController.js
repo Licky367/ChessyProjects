9fbd85914ac22fff8a6f01916172ed843c34e271
Licky367 <licky367@gmail.com>
2026-05-03 22:48:21 +0300

    Update cageController.js

--- controllers/cageController.js
+++ controllers/cageController.js
@@ -1,17 +1,20 @@
+// controllers/cageController.js
+
 const cageService = require("../services/cageService");
 
 exports.renderCageList = async (req, res) => {
   const cages = await cageService.getCages();
-
   res.render("cage/list", { cages });
 };
 
 exports.renderCageDetails = async (req, res) => {
   const cage = await cageService.getCageById(req.params.id);
-
   res.render("cage/details", { cage });
 };
 
+// =========================
+// COLLECT EGGS
+// =========================
 exports.collectEggs = async (req, res) => {
   try {
     const { eggs } = req.body;
@@ -23,10 +26,13 @@ exports.collectEggs = async (req, res) => {
 
     res.redirect(`/cage/${req.params.id}`);
   } catch (err) {
-    res.send(err.message);
+    res.status(400).send(err.message);
   }
 };
 
+// =========================
+// SELL CHICKEN
+// =========================
 exports.sellChicken = async (req, res) => {
   try {
     const { count, amount } = req.body;
@@ -40,6 +46,25 @@ exports.sellChicken = async (req, res) => {
 
     res.redirect("/cage");
   } catch (err) {
-    res.send(err.message);
+    res.status(400).send(err.message);
+  }
+};
+
+// =========================
+// DEAD CHICKEN
+// =========================
+exports.recordDeadChicken = async (req, res) => {
+  try {
+    const { count } = req.body;
+
+    await cageService.recordDeadChicken({
+      cageId: req.params.id,
+      count,
+      user: req.user
+    });
+
+    res.redirect(`/cage/${req.params.id}`);
+  } catch (err) {
+    res.status(400).send(err.message);
   }
 };
