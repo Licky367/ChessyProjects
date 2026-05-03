3d3a4dd9ba1416c1d931abe9d11f86323a5f4df8
Licky367 <licky367@gmail.com>
2026-05-03 23:10:34 +0300

    Update poultryNursing.js

--- routes/poultryNursing.js
+++ routes/poultryNursing.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/nursingController");
 
-router.get("/", controller.renderNursingList);
+const nursingController = require("../controllers/nursingController");
 
-router.post("/purchase", controller.createPurchase);
+// existing routes
+router.get("/", nursingController.renderNursingList);
+router.post("/purchase", nursingController.createPurchase);
 
-router.get("/:id", controller.renderBatchDetails);
+router.get("/:id", nursingController.renderBatchDetails);
 
-router.post("/:id/death", controller.recordDeath);
-router.post("/:id/sell", controller.sellPoultry);
-router.post("/:id/cage", controller.cageBatch);
+router.post("/:id/death", nursingController.recordDeath);
+router.post("/:id/sell", nursingController.sellPoultry);
+router.post("/:id/cage", nursingController.cageBatch);
+
+// NEW ROUTE (IMPORTANT)
+router.post("/:id/eggs", nursingController.collectEggs);
 
 module.exports = router;
