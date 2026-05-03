7dbbd5b21cf4b0739f381c08b3a3c22e3d565f4e
Licky367 <licky367@gmail.com>
2026-05-04 01:19:38 +0300

    Update poultryStatsController.js

--- controllers/poultryStatsController.js
+++ controllers/poultryStatsController.js
@@ -1,16 +1,22 @@
 const service = require("../services/poultryStatsService");
 
 exports.renderStats = async (req, res) => {
-  const { date, type } = req.query;
+  try {
+    const { date, type } = req.query;
 
-  const data = await service.getStats({
-    date,
-    type: type || "eggs"
-  });
+    const data = await service.getStats({
+      date,
+      type: type || "eggs"
+    });
 
-  res.render("poultryStats", {
-    ...data,
-    selectedDate: date,
-    selectedType: type || "eggs"
-  });
+    res.render("poultryStats", {
+      rows: data.rows || [],
+      grandTotal: data.grandTotal || { day: 0, month: 0, year: 0 },
+      selectedDate: date || "",
+      selectedType: type || "eggs"
+    });
+
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
