4001ee02309ee97f13680a44ff2dc84a5650fca5
Licky367 <licky367@gmail.com>
2026-05-04 08:07:12 +0300

    Update poultryStatsController.js

--- controllers/poultryStatsController.js
+++ controllers/poultryStatsController.js
@@ -1,19 +1,29 @@
 const service = require("../services/poultryStatsService");
 
+const VALID_TYPES = [
+  "eggs",
+  "egg_sales",
+  "poultry_sales",
+  "total_revenue",
+  "profit"
+];
+
 exports.renderStats = async (req, res) => {
   try {
     const { date, type } = req.query;
 
+    const selectedType = VALID_TYPES.includes(type) ? type : "eggs";
+
     const data = await service.getStats({
       date,
-      type: type || "eggs"
+      type: selectedType
     });
 
     res.render("poultryStats", {
       rows: data.rows || [],
       grandTotal: data.grandTotal || { day: 0, month: 0, year: 0 },
       selectedDate: date || "",
-      selectedType: type || "eggs"
+      selectedType
     });
 
   } catch (err) {
