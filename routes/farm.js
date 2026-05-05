7fe578d7700136ee93b74a54a157bc458a53681a
Licky367 <licky367@gmail.com>
2026-05-05 21:53:47 +0300

    Update farm.js

--- routes/farm.js
+++ routes/farm.js
@@ -2,20 +2,23 @@ const express = require('express');
 const router = express.Router();
 const farmController = require('../controllers/farmController');
 
-// Form to create project
+// Form to create a project
 router.get('/', farmController.showCreateForm);
 router.post('/create', farmController.createProject);
 
 // List of all projects
 router.get('/projects', farmController.listProjects);
 
-// Project details
+// Project details page
 router.get('/details', farmController.showProjectDetails);
 
-// Begin project
+// Mark project as Begun
 router.post('/begin', farmController.beginProject);
 
-// Add additional cost
+// Mark project as Completed
+router.post('/complete', farmController.completeProject);
+
+// Add additional cost or income
 router.post('/add-cost', farmController.addCost);
 
 module.exports = router;
