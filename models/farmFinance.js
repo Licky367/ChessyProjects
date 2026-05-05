c211f5793dbe71b421775438e9eb922f7d02d621
Licky367 <licky367@gmail.com>
2026-05-05 21:51:28 +0300

    Update farmFinance.js

--- models/farmFinance.js
+++ models/farmFinance.js
@@ -15,6 +15,11 @@ const farmFinanceSchema = new mongoose.Schema({
     type: Number,
     required: true,
   },
+  type: {  // <-- NEW
+    type: String,
+    enum: ['inflow', 'outflow'],
+    required: true,
+  },
   dateIncurred: {
     type: Date,
     default: Date.now,
